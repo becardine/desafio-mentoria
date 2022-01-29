c0721f125de3fda5619b583e7f7845db94569043
becardine <brenda_alves@live.com>
2022-01-29 13:33:09 -0300

    Criando dark mode

---
+++ src/js/function.js
@@ -0,0 +1,8 @@
+//Dark Mode
+
+const $html = document.querySelector('html')
+const $checkbox = document.querySelector('#switch')
+
+$checkbox.addEventListener('change', function(){
+  $html.classList.toggle('dark-mode')
+})
